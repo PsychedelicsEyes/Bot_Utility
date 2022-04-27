9b7c1b7550c82a6ebbb5d7a925095ed5de06798d
PsychedelicsEyes <75976563+PsychedelicsEyes@users.noreply.github.com>
2022-04-27 18:22:07 +0200

    Update client.js

--- src/Structure/Config/client.js
+++ src/Structure/Config/client.js
@@ -2,7 +2,7 @@ module.exports = {
     token: "Your token",
     prefix: "Your prefix",
     mongoURL: "Your url mongodb",
-    owner: 'Your id',
+    owner: 'Your id',//Mandatory
     thumbnailActive: 'True or false' //If you want to activate the thumbnail on embeds messages
 }
 //Copyright PsychedelicsEyes
